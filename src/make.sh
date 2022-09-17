42ea5c75d550071b2fb13b86a2debb3983c13b94
dem123456789 <diao_em@hotmail.com>
2022-09-17 06:22:41 -0400

    update

--- src/make.sh
+++ src/make.sh
@@ -6,7 +6,7 @@ num_gpus=1
 round=8
 num_experiments=2
 
-for data_name in ${modes[@]}; do
-    python make.py --mode base --run train --resume_mode $resume_mode --num_gpus $num_gpus --round $round --num_experiments $num_experiments
-    python make.py --mode base --run test --resume_mode $resume_mode --num_gpus $num_gpus --round $round --num_experiments $num_experiments
+for mode in ${modes[@]}; do
+    python make.py --mode $mode --run train --resume_mode $resume_mode --num_gpus $num_gpus --round $round --num_experiments $num_experiments
+    python make.py --mode $mode --run test --resume_mode $resume_mode --num_gpus $num_gpus --round $round --num_experiments $num_experiments
   done
