cb038e0bc7cdd21697bc60100f56658de2a25314
dem123456789 <diao_em@hotmail.com>
2025-07-08 00:48:23 +0800

    update code

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
     process_control()
     with torch.no_grad():
         for data_name in data_names:
-            dataset = make_dataset(data_name, transform=False, process=True)
-            dataset['train'].transform = Compose([transforms.ToTensor()])
+            dataset = make_dataset(data_name, process=True)
             process_dataset(dataset)
             cfg['step'] = 0
             data_loader = make_data_loader(dataset, cfg[cfg['tag']]['optimizer']['batch_size'], shuffle=False)
--- src/model/base.py
+++ src/model/base.py
@@ -44,23 +44,3 @@ class Base(nn.Module):
 
 def base(model, data_name, stats):
     return Base(model, data_name, stats)
-
-
-
-
-# class Base(nn.Module):
-#     def __init__(self, model):
-#         super().__init__()
-#         self.model = model
-#         self.loss = make_loss
-#
-#     def forward(self, **input):
-#         output = {}
-#         output['pred'] = self.model(input['data'])
-#         output['loss'] = self.loss(output, input)
-#         return output
-#
-#
-# def base(model):
-#     model = Base(model)
-#     return model
